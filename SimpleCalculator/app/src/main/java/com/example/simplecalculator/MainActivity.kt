d98ed5fa91851439df54f08a65cb6d7cabf3fe53
yujinK <yujinkim.dev@gmail.com>
2019-06-10 19:44:41 +0900

    feat: 더하기 기능 구현

--- SimpleCalculator/app/src/main/java/com/example/simplecalculator/MainActivity.kt
+++ SimpleCalculator/app/src/main/java/com/example/simplecalculator/MainActivity.kt
@@ -2,11 +2,167 @@ package com.example.simplecalculator
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    lateinit var txtResult: TextView
+    lateinit var btnZero: Button
+    lateinit var btnOne: Button
+    lateinit var btnTwo: Button
+    lateinit var btnThree: Button
+    lateinit var btnFour: Button
+    lateinit var btnFive: Button
+    lateinit var btnSix: Button
+    lateinit var btnSeven: Button
+    lateinit var btnEight: Button
+    lateinit var btnNine: Button
+    lateinit var btnClear: Button
+    lateinit var btnSign: Button
+    lateinit var btnDot: Button
+    lateinit var btnPlus: Button
+    var operator: String = ""
+    var operand: String = ""
+    var calculationResult: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initView()
+        setOnClicks()
     }
+
+    override fun onClick(v: View) {
+        var result: String = txtResult.text.toString()
+        if (result.length == 1 && result.equals("0")) {
+            operand = ""
+        }
+
+        when (v.id) {
+            R.id.btn_zero -> {
+                operand += "0"
+                txtResult.text = operand
+            }
+            R.id.btn_one -> {
+                operand += "1"
+                txtResult.text = operand
+            }
+            R.id.btn_two -> {
+                operand += "2"
+                txtResult.text = operand
+            }
+            R.id.btn_three -> {
+                operand += "3"
+                txtResult.text = operand
+            }
+            R.id.btn_four -> {
+                operand += "4"
+                txtResult.text = operand
+            }
+            R.id.btn_five -> {
+                operand += "5"
+                txtResult.text = operand
+            }
+            R.id.btn_six -> {
+                operand += "6"
+                txtResult.text = operand
+            }
+            R.id.btn_seven -> {
+                operand += "7"
+                txtResult.text = operand
+            }
+            R.id.btn_eight -> {
+                operand += "8"
+                txtResult.text = operand
+            }
+            R.id.btn_nine -> {
+                operand += "9"
+                txtResult.text = operand
+            }
+            R.id.btn_clear -> {
+                operand = "0"
+                txtResult.text = operand
+                calculationResult = 0
+            }
+            R.id.btn_dot ->
+                if (operand.isEmpty()) {
+                    txtResult.text = "0."
+                } else {
+                    txtResult.text = operand + "."
+                }
+            R.id.btn_plus -> {
+                if (operator.isNotEmpty()) {
+                    calculate()
+                    txtResult.text = calculationResult.toString()
+                } else {
+                    calculationResult = operand.toInt()
+                }
+
+                operator = "+"
+                operand = ""
+            }
+        }
+    }
+
+    private fun calculate() {
+        when (operator) {
+            "+" -> calculationResult += operand.toInt()
+            "-" -> calculationResult -= operand.toInt()
+            "*" -> calculationResult *= operand.toInt()
+            "/" -> calculationResult /= operand.toInt()
+        }
+    }
+
+    private fun initView() {
+        txtResult = findViewById(R.id.txt_result)
+        btnZero = findViewById(R.id.btn_zero)
+        btnOne = findViewById(R.id.btn_one)
+        btnTwo = findViewById(R.id.btn_two)
+        btnThree = findViewById(R.id.btn_three)
+        btnFour = findViewById(R.id.btn_four)
+        btnFive = findViewById(R.id.btn_five)
+        btnSix = findViewById(R.id.btn_six)
+        btnSeven = findViewById(R.id.btn_seven)
+        btnEight = findViewById(R.id.btn_eight)
+        btnNine = findViewById(R.id.btn_nine)
+        btnClear = findViewById(R.id.btn_clear)
+        btnSign = findViewById(R.id.btn_sign)
+        btnDot = findViewById(R.id.btn_dot)
+        btnPlus = findViewById(R.id.btn_plus)
+    }
+
+    private fun setOnClicks() {
+        btnZero.setOnClickListener(this)
+        btnOne.setOnClickListener(this)
+        btnTwo.setOnClickListener(this)
+        btnThree.setOnClickListener(this)
+        btnFour.setOnClickListener(this)
+        btnFive.setOnClickListener(this)
+        btnSix.setOnClickListener(this)
+        btnSeven.setOnClickListener(this)
+        btnEight.setOnClickListener(this)
+        btnNine.setOnClickListener(this)
+        btnClear.setOnClickListener(this)
+        btnSign.setOnClickListener(this)
+        btnDot.setOnClickListener(this)
+        btnPlus.setOnClickListener(this)
+    }
+
+    //TODO: Number Format - Comma
+//    private fun setNumberFormat(s: String): String {
+//        var currentLocale: Locale = getCurrentLocales(applicationContext)
+//        var str: String = NumberFormat.getNumberInstance(currentLocale).format(Integer.parseInt(s))
+//
+//        return str
+//    }
+//
+//    private fun getCurrentLocales(context: Context): Locale {
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+//            return context.resources.configuration.locales.get(0)
+//        } else {
+//            return context.resources.configuration.locale
+//        }
+//    }
 }
